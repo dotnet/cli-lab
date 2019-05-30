deb6d843919cd4af1a6b5c95a665b41bf879b235
William Li <wul@microsoft.com>
2019-05-29 22:41:41 -0700

    Hello world with Arcade

---
+++ src/dotnet-uninstall/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.DotNet.Tools.Uninstall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Messages.message);
+        }
+    }
+}
