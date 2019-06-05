c8ba063bb57d597159c545104dddf57df3a56ee3
Yuchong Pan <panyuchong@gmail.com>
2019-06-05 09:52:51 -0700

    Added command line parser

--- src/dotnet-uninstall/Program.cs
+++ src/dotnet-uninstall/Program.cs
@@ -2,14 +2,33 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 
 namespace Microsoft.DotNet.Tools.Uninstall
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine(Messages.message);
+            var rootCommand = new RootCommand("dotnet-uninstall");
+
+            var listCommand = new Command("list");
+            var uninstallCommand = new Command("uninstall");
+            rootCommand.Add(listCommand);
+            rootCommand.Add(uninstallCommand);
+
+            listCommand.Handler = CommandHandler.Create(() =>
+            {
+                Console.WriteLine("invoked list subcommand"); // TODO: implement list
+            });
+
+            uninstallCommand.Handler = CommandHandler.Create(() =>
+            {
+                Console.WriteLine("invoked uninstall subcommand"); // TODO: implement uninstall
+            });
+
+            return rootCommand.InvokeAsync(args).Result;
         }
     }
 }
