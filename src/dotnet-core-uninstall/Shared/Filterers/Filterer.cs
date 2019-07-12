54631a035f4b47b93fd3dbe84272fbe1aebd551e
Yuchong Pan <panyuchong@gmail.com>
2019-07-12 01:17:14 -0700

    Added ASP.NET Runtimes in no-argument filterers

--- src/dotnet-core-uninstall/Shared/Filterers/Filterer.cs
+++ src/dotnet-core-uninstall/Shared/Filterers/Filterer.cs
@@ -88,6 +88,7 @@ namespace Microsoft.DotNet.Tools.Uninstall.Shared.Filterers
 
             var sdks = Bundle<SdkVersion>.FilterWithSameBundleType(filteredBundlesByArch);
             var runtimes = Bundle<RuntimeVersion>.FilterWithSameBundleType(filteredBundlesByArch);
+            var aspNetRuntimes = Bundle<AspNetRuntimeVersion>.FilterWithSameBundleType(filteredBundlesByArch);
 
             var filteredSdks = typeSelection.HasFlag(BundleType.Sdk) ?
                 Filter(sdks).OrderBy(sdk => sdk).Select(sdk => sdk as Bundle) :
@@ -97,7 +98,11 @@ namespace Microsoft.DotNet.Tools.Uninstall.Shared.Filterers
                 Filter(runtimes).OrderBy(runtime => runtime).Select(runtime => runtime as Bundle) :
                 new List<Bundle>();
 
-            return filteredSdks.Concat(filteredRuntimes);
+            var filteredAspNetRuntimes = typeSelection.HasFlag(BundleType.AspNetRuntime) ?
+                Filter(aspNetRuntimes).OrderBy(aspNetRuntime => aspNetRuntime).Select(aspNetRuntime => aspNetRuntime as Bundle) :
+                new List<Bundle>();
+
+            return filteredSdks.Concat(filteredRuntimes).Concat(filteredAspNetRuntimes);
         }
 
         public abstract IEnumerable<Bundle<TBundleVersion>> Filter<TBundleVersion>(IEnumerable<Bundle<TBundleVersion>> bundles)
