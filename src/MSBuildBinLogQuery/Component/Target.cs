e76a8be1b9d7d3a1e606e6974d6f2091fbebdca2
Yuchong Pan <panyuchong@gmail.com>
2019-08-08 14:06:16 -0700

    Added Target.OrderedTasks

--- src/MSBuildBinLogQuery/Component/Target.cs
+++ src/MSBuildBinLogQuery/Component/Target.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using Microsoft.Build.Logging.Query.Graph;
 
 namespace Microsoft.Build.Logging.Query.Component
@@ -8,6 +9,8 @@ namespace Microsoft.Build.Logging.Query.Component
         public string Name { get; }
         public Project Parent { get; }
         public ConcurrentDictionary<int, Task> Tasks { get; }
+        // TODO: Update the AddOrGetTask(int) method when PR #72 gets merged.
+        public List<Task> OrderedTasks { get; }
         public TargetNode_BeforeThis Node_BeforeThis { get; }
         public TargetNode_AfterThis Node_AfterThis { get; }
 
@@ -16,6 +19,7 @@ namespace Microsoft.Build.Logging.Query.Component
             Name = name;
             Parent = parent;
             Tasks = new ConcurrentDictionary<int, Task>();
+            OrderedTasks = new List<Task>();
             Node_BeforeThis = new TargetNode_BeforeThis(this);
             Node_AfterThis = new TargetNode_AfterThis(this);
         }
