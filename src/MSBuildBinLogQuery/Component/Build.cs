95acbcac12e61eacb3a7875e793faaff8c9cd31b
Yuchong Pan <panyuchong@gmail.com>
2019-08-09 11:16:14 -0700

    Dropped Build.OrderedProjects

--- src/MSBuildBinLogQuery/Component/Build.cs
+++ src/MSBuildBinLogQuery/Component/Build.cs
@@ -6,32 +6,22 @@ namespace Microsoft.Build.Logging.Query.Component
     public class Build : Component
     {
         public ConcurrentDictionary<int, Project> Projects { get; }
-        public ConcurrentBag<Project> OrderedProjects { get; }
 
         public Build() : base()
         {
             Projects = new ConcurrentDictionary<int, Project>();
-            OrderedProjects = new ConcurrentBag<Project>();
         }
 
         public Project GetOrAddProject(int id, ProjectStartedEventArgs args)
         {
-            var project = new Project(
+            return Projects.GetOrAdd(id, new Project(
                 id,
                 args.ProjectFile,
                 args.TargetNames,
                 args.Items,
                 args.Properties,
                 args.GlobalProperties,
-                this);
-
-            if (Projects.TryAdd(id, project))
-            {
-                OrderedProjects.Add(project);
-                return project;
-            }
-
-            return Projects[id];
+                this));
         }
     }
 }
