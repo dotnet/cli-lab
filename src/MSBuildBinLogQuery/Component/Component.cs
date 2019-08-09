5680d3ae22a8403e2c00841687ea35153ccb3d1e
Yuchong Pan <panyuchong@gmail.com>
2019-08-09 15:29:39 -0700

    Added computation of Component.AllMessages, Component.AllWarnings and Component.AllErrors

--- src/MSBuildBinLogQuery/Component/Component.cs
+++ src/MSBuildBinLogQuery/Component/Component.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.Build.Logging.Query.Messaging;
 
@@ -9,31 +10,58 @@ namespace Microsoft.Build.Logging.Query.Component
         public IReadOnlyList<Message> Messages => _messages;
         public IReadOnlyList<Warning> Warnings => _warnings;
         public IReadOnlyList<Error> Errors => _errors;
+        
+        /// <summary>
+        /// `AllMessages`, `AllWarnings` and `AllErrors` are messages, warnings
+        /// and errors that occur under this component, either directly or
+        /// indirectly.
+        /// </summary>
+        public IReadOnlyList<Message> AllMessages => _allMessages;
+        public IReadOnlyList<Warning> AllWarnings => _allWarnings;
+        public IReadOnlyList<Error> AllErrors => _allErrors;
 
         private readonly List<Message> _messages;
         private readonly List<Warning> _warnings;
         private readonly List<Error> _errors;
+        private readonly List<Message> _allMessages;
+        private readonly List<Warning> _allWarnings;
+        private readonly List<Error> _allErrors;
 
         public Component()
         {
             _messages = new List<Message>();
             _warnings = new List<Warning>();
             _errors = new List<Error>();
+
+            _allMessages = new List<Message>();
+            _allWarnings = new List<Warning>();
+            _allErrors = new List<Error>();
         }
 
         public void AddMessage(Message message)
         {
             _messages.Add(message);
+            GoUpToRoot(component => component._allMessages.Add(message));
         }
 
         public void AddWarning(Warning warning)
         {
             _warnings.Add(warning);
+            GoUpToRoot(component => component._allWarnings.Add(warning));
         }
 
         public void AddError(Error error)
         {
             _errors.Add(error);
+            GoUpToRoot(component => component._allErrors.Add(error));
+        }
+
+        private void GoUpToRoot(Action<Component> action)
+        {
+            for (var current = this; current != null; current = current.Parent)
+            {
+                action.Invoke(current);
+            }
         }
     }
 }
