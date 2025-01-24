7220c30aab214545ad52a201f20623b396b50769
Eduardo Villalpando Mello <eduardov@microsoft.com>
2025-01-24 13:34:40 -0800

    Update MacOsBundleDisplayNameFormat to show correct arch type

--- src/dotnet-core-uninstall/LocalizableStrings.Designer.cs
+++ src/dotnet-core-uninstall/LocalizableStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.DotNet.Tools.Uninstall {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class LocalizableStrings {
@@ -291,7 +291,7 @@ namespace Microsoft.DotNet.Tools.Uninstall {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Microsoft .NET Core {0} {1} (x64).
+        ///   Looks up a localized string similar to Microsoft .NET Core {0} {1} ({2}).
         /// </summary>
         internal static string MacOsBundleDisplayNameFormat {
             get {
