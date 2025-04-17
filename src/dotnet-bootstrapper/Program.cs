8c10e2dfea2a9572ca96ab16092bb399f0fee179
Eduardo Villalpando Mello <eduardov@microsoft.com>
2025-04-16 17:07:41 -0700

    Use System.CommandLine

--- src/dotnet-bootstrapper/Program.cs
+++ src/dotnet-bootstrapper/Program.cs
@@ -1,13 +1,30 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.DotNet.Tools.Bootstrapper
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using System.IO;
+using System.Threading.Tasks;
+namespace Microsoft.DotNet.Tools.Bootstrapper;
+
+class Program
 {
-    internal class Program
+    static int Main(string[] args)
     {
-        internal static int Main(string[] args)
+        var rootCommand = new RootCommand("dotnet bootstrapper");
+
+        var installCommand = new Command("install", "Install the specified version of the .NET SDK.")
         {
-            return 0;
-        }
+            new Argument<string>("version")
+            {
+                Description = "The version of the .NET SDK to install.",
+                Arity = ArgumentArity.ExactlyOne
+            }
+        };
+
+        rootCommand.AddCommand(installCommand);
+
+        return rootCommand.Invoke(args);
     }
 }
