5ad97794cd96f49fef5653e26c681085e6f5d760
Eduardo Villalpando Mello <eduardov@microsoft.com>
2025-04-21 15:28:39 -0700

    [Bootstrapper] System.CommandLine: Add help text and command (#388)
    * Remove LocalizableStrings.Designer.cs
    
    * Use System.CommandLine
    
    * Add real command parser
    
    ---------
    
    Co-authored-by: Forgind <12969783+Forgind@users.noreply.github.com>

--- src/dotnet-bootstrapper/Program.cs
+++ src/dotnet-bootstrapper/Program.cs
@@ -1,13 +1,17 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.DotNet.Tools.Bootstrapper
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using System.IO;
+using System.Threading.Tasks;
+namespace Microsoft.DotNet.Tools.Bootstrapper;
+
+class Program
 {
-    internal class Program
+    static int Main(string[] args)
     {
-        internal static int Main(string[] args)
-        {
-            return 0;
-        }
+        return BootstrapperCommandParser.BootstrapParser.InvokeAsync(args).Result;
     }
 }
