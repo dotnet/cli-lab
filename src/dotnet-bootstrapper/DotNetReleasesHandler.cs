b6b249cbba09abfdc4b1f3be3015014d9eddfdf7
Eduardo Villalpando Mello <eduardov@microsoft.com>
2025-04-23 13:35:37 -0700

    Add Packages

---
+++ src/dotnet-bootstrapper/DotNetReleasesHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;  
+using Microsoft.Deployment.DotNet.Releases;
+
+namespace Microsoft.DotNet.Tools.Bootstrapper;
+
+internal static class DotNetReleasesHandler
+{
+    internal static Uri[] DotNetReleasesIndexFeeds = new Uri[]
+    {
+            new("https://builds.dotnet.microsoft.com/dotnet/release-metadata/releases-index.json"),
+            new("https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/2.1/releases.json")
+    };
+
+    internal static Uri DotNetRelesesMetadataFeed(string channelVersion) =>
+        new($"https://builds.dotnet.microsoft.com/dotnet/release-metadata/{channelVersion}/releases.json");
+}
