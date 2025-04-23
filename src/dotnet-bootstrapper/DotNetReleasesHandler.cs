c623aa75d97aeaa25605f64d9c82a92dff65a22c
Eduardo Villalpando Mello <eduardov@microsoft.com>
2025-04-23 16:52:44 -0700

    Add DotNetReleasesHandler

--- src/dotnet-bootstrapper/DotNetReleasesHandler.cs
+++ src/dotnet-bootstrapper/DotNetReleasesHandler.cs
@@ -1,16 +1,17 @@
-﻿using System;  
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Deployment.DotNet.Releases;
 
 namespace Microsoft.DotNet.Tools.Bootstrapper;
 
 internal static class DotNetReleasesHandler
 {
-    internal static Uri[] DotNetReleasesIndexFeeds = new Uri[]
+    public static async Task<Product> GetChannelAsync(string channelVersion)
     {
-            new("https://builds.dotnet.microsoft.com/dotnet/release-metadata/releases-index.json"),
-            new("https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/2.1/releases.json")
-    };
-
-    internal static Uri DotNetRelesesMetadataFeed(string channelVersion) =>
-        new($"https://builds.dotnet.microsoft.com/dotnet/release-metadata/{channelVersion}/releases.json");
+        ProductCollection productCollection = await ProductCollection.GetAsync();
+        return productCollection.FirstOrDefault((Product product) => product.ProductVersion.Equals(channelVersion));
+    }
 }
